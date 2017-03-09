b1ade28b1face017b1d76f3e8dfed6c9ed9eb28c
liyouzhi <lyz@buaa.us>
2017-03-09 17:47:44 +0800

    added some templates and fixed some bug..the web app have complected basically...

--- www/handlers.py
+++ www/handlers.py
@@ -69,7 +69,7 @@ async def cookie2user(cookie_str):
         return None
 
 @get('/')
-def index(*, page='1'):
+async def index(*, page='1'):
     page_index = get_page_index(page)
     num = await Blog.findNumber('count(id)')
     page = Page(num)
@@ -148,7 +148,7 @@ def manage():
 @get('/manage/comments')
 def manage_comments(*, page='1'):
     return {
-            '__template__': 'manage_comments.html'
+            '__template__': 'manage_comments.html',
             'page_index': get_page_index(page)
             }
 
@@ -193,11 +193,11 @@ async def api_comments(*, page='1'):
     return dict(page=p, comments=comments)
 
 @post('/api/blogs/{id}/comments')
-def api_create_comment(id, request, *, content):
+async def api_create_comment(id, request, *, content):
     user = request.__user__
     if user is None:
         raise APIPermissionError('Please signin first.')
-    if not content or content.strip():
+    if not content or not content.strip():
         raise APIValueError('content')
     blog = await Blog.find(id)
     if blog is None:
@@ -207,7 +207,7 @@ def api_create_comment(id, request, *, content):
     return comment
 
 @post('/api/comments/{id}/delete')
-def api_delete_comments(id, request):
+async def api_delete_comments(id, request):
     check_admin(request)
     c = await Comment.find(id)
     if c is None:
@@ -216,7 +216,7 @@ def api_delete_comments(id, request):
     return dict(id=id)
 
 @get('/api/users')
-def api_get_users(*, page='1'):
+async def api_get_users(*, page='1'):
     page_index = get_page_index(page)
     num = await User.findNumber('count(id)')
     p = Page(num, page_index)
@@ -281,3 +281,25 @@ async def api_create_blog(request, *, name, summary, content):
     await blog.save()
     return blog
 
+@post('/api/blogs/{id}')
+async def api_update_blog(id, request, *, name, summary, content):
+    check_admin(request)
+    blog = await Blog.find(id)
+    if not name or not name.strip():
+        raise APIValueError('name', 'name cannot be empty.')
+    if not summary or not summary.strip():
+        raise APIValueError('summary', 'summary cannot be empty.')
+    if not content or not content.strip():
+        raise APIValueError('content', 'content cannot be empty.')
+    blog.name = name.strip()
+    blog.summary = summary.strip()
+    blog.content = content.strip()
+    await blog.update()
+    return blog
+
+@post('/api/blogs/{id}/delete')
+async def api_delete_blog(request, *, id):
+    check_admin(request)
+    blog = await Blog.find(id)
+    await blog.remove()
+    return dict(id=id)
